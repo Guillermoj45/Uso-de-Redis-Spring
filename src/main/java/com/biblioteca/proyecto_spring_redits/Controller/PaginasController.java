69e2b100f5ca1cb2c942e9d15a9526e759e608ab
guill <guillermoremesa@gmail.com>
2024-11-24 22:00:00 +0100

    Demasiados arreglos como para ponerlos en un comentario

--- src/main/java/com/biblioteca/proyecto_spring_redits/Controller/PaginasController.java
+++ src/main/java/com/biblioteca/proyecto_spring_redits/Controller/PaginasController.java
@@ -1,20 +1,50 @@
 package com.biblioteca.proyecto_spring_redits.Controller;
 
+import com.biblioteca.proyecto_spring_redits.Service.ClienteService;
+import com.biblioteca.proyecto_spring_redits.Service.EmpleadosService;
 import com.biblioteca.proyecto_spring_redits.Service.MenuService;
 import lombok.AllArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @AllArgsConstructor
 @Controller
 public class PaginasController {
     private final MenuService menuService;
+    private final ClienteService clienteService;
+    private final EmpleadosService empleadoService;
 
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("menus", menuService.listAll());
+        return "index";
+    }
+
+    @GetMapping("clientes")
+    public String clientes() {
+
+        return "index";
+    }
+    @GetMapping("cliente/{id}")
+    public String clientes(@PathVariable int id) {
+        return "cliente";
+    }
+
+    @GetMapping("empleados")
+    public String empleados() {
+        return "index";
+    }
+
+    @GetMapping("empleado/{id}")
+    public String empleados(@PathVariable int id) {
+        return "empleado";
+    }
+
+    @GetMapping("compras")
+    public String compras() {
         return "index";
     }
 }
