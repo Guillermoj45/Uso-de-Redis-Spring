3ea1a968365d219369dff3fb593c81bd8bf9351e
guill <guillermoremesa@gmail.com>
2024-11-28 19:30:08 +0100

    Arreglos de empleados

--- src/main/java/com/biblioteca/proyecto_spring_redits/Controller/EmpleadosController.java
+++ src/main/java/com/biblioteca/proyecto_spring_redits/Controller/EmpleadosController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/empleado")
+@RequestMapping("/empleados")
 
 public class EmpleadosController {
 
@@ -25,20 +25,52 @@ public class EmpleadosController {
         this.passwordEncoder = passwordEncoder;
     }
 
+
     @GetMapping
-    public List<Empleado> listAll() {
-        return empleadosService.listAll();
+    public String empleados(Model model){
+        model.addAttribute("empleados", empleadosService.listAll());
+        return "Pantallas/Empleados";
     }
 
-    @GetMapping("{id}")
-    public Empleado findById(@PathVariable int id) {
-        return empleadosService.findById(id);
+    @GetMapping("/nuevo")
+    public String mostrarFormularioDeRegistro(Model model) {
+        model.addAttribute("empleado", new Empleado());
+        return "Registro/modificacion/RegistroEmpleados";
     }
 
     @PostMapping
-    public Empleado create(@RequestBody Empleado empleado) {
-        return empleadosService.save(empleado);
+    public String registrarEmpleado(Empleado empleado) {
+        if (empleado.getId() != 0) {
+            empleado.setPassword(passwordEncoder.encode(empleado.getPassword()));
+        }
+        if (empleado.getPassword().isBlank() && empleado.getId() != 0) {
+            empleado.setPassword(empleadosService.findById(empleado.getId()).getPassword());
+        }
+        empleadosService.save(empleado);
+        return "redirect:/empleados";
     }
+
+    @GetMapping("/edit/{id}")
+    public String mostrarFormularioModificacion(Model model, @PathVariable int id) {
+        model.addAttribute("empleado", empleadosService.findById(id));
+        return "Registro/modificacion/ModificarEmpleados";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String mostrarFormularioModificacionRecepcion(Empleado empleado) {
+        if (empleado.getId() != 0) {
+            Empleado existingEmpleado = empleadosService.findById(empleado.getId());
+            if (existingEmpleado != null) {
+                empleado.setPassword(existingEmpleado.getPassword());
+            }
+        } else {
+            empleado.setPassword(passwordEncoder.encode(empleado.getPassword()));
+        }
+        empleadosService.save(empleado);
+        return "redirect:/empleado";
+    }
+
+
 
     @PutMapping
     public Empleado update(@RequestBody Empleado empleado) {
